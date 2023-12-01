aa7618cd36d0a0c7f904eb4e09fdc5611aaf89d7
PJ02 <parthljain2002@gmail.com>
2023-12-02 00:48:20 +0530

    Added Pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -10,6 +10,9 @@ import ChatInterface from './components/ChatInterface'
 import IdeaInterface from './components/IdeaInterface'
 import LandingPage from './components/LandingPage'
 import { Navigate } from 'react-router-dom'
+import MarketInsight from './components/MarketInsight'
+import VisionDoc from './components/VisionDoc'
+import Collab from './components/Collab'
 
 const ProtectedRoute = ({children}) => {
   const isLoggedIn = localStorage.getItem("ideagen_logged_in")
@@ -30,6 +33,10 @@ export default function App() {
             <Route path="/research" element={<ProtectedRoute><ResearchBank /></ProtectedRoute>} />
             <Route path="/chat/:chatid" element={<ProtectedRoute><ChatInterface /></ProtectedRoute>} />
             <Route path="/idea/:ideaid" element={<ProtectedRoute><IdeaInterface/></ProtectedRoute>} />
+
+            <Route path="/market-insight/:ideaid" element={<ProtectedRoute><MarketInsight /></ProtectedRoute>} />
+            <Route path="/vision-doc/:ideaid" element={<ProtectedRoute><VisionDoc /></ProtectedRoute>} />
+            <Route path="/collab/:ideaid" element={<ProtectedRoute><Collab/></ProtectedRoute>} />
           </Routes>
         </BrowserRouter>
       </AuthProvider>
---
+++ Frontend/src/components/VisionDoc.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function VisionDoc() {
+  return (
+    <div>VisionDoc</div>
+  )
+}
+
+export default VisionDoc
