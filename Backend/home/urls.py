99bd81da29494d0b9d880baed58580f642023265
PJ02 <parthljain2002@gmail.com>
2023-12-06 18:36:04 +0530

    Vision Doc locked

--- Backend/home/urls.py
+++ Backend/home/urls.py
@@ -19,6 +19,7 @@ urlpatterns = [
     path('get_user',views.get_user, name='get_user'),
     path('new_user',views.new_user, name='new_user'),
     path('get_topics',views.get_topics, name='get_topics'),
+    path('get_topics_details',views.get_topics_details, name='get_topics_details'),
     path('get_topic',views.get_topic, name='get_topic'),
     path('new_topic',views.new_topic, name='new_topic'),
     path('edit_topic',views.edit_topic, name='edit_topic'),
