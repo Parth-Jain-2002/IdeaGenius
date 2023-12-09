8530f857b612a29132b477921fa2a88d69374215
Nalin Angrish <nalin@nalinangrish.me>
2023-12-09 20:55:14 +0530

    Minor Fix

--- Frontend/src/components/auth/ForgotPassword.jsx
+++ Frontend/src/components/auth/ForgotPassword.jsx
@@ -26,7 +26,7 @@ export default function ForgotPassword() {
         </div>
 
         <div
-          className="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 xl:w-2/5 h-screen px-6 lg:px-16 xl:px-12
+          className="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:w-1/2 xl:w-2/5 h-screen px-6 lg:px-16 xl:px-12
                     flex items-center justify-center"
         >
           <div className="w-full h-100">
